81e57d3b880eafae5a7a17120ee5eba0d4faf893
e <ericoporto2008@gmail.com>
2024-05-29 20:35:20 -0300

    add initial version win and linux

--- main.cpp
+++ main.cpp
@@ -1,6 +1,53 @@
 #include <iostream>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+#ifdef _WIN32
+#include "Windows.h"
+
+int GetFreeSizeInMB(const std::string &path)
+{
+    const uint64_t MB = 1024 * 1024;
+    uint64_t freeBytes;
+    LPCSTR p = path.c_str();
+    if(GetDiskFreeSpaceEx (p, (PULARGE_INTEGER)&freeBytes,
+                                   nullptr,
+                                   nullptr) != 0)
+    {
+        return static_cast<int>(freeBytes / MB);
+    }
+
+    return 0;
+}
+#else
+// assume Linux for now
+#include <unistd.h>
+#include <stdbool.h>
+#include <sys/statvfs.h>
+
+unsigned long rounddiv(unsigned long num, unsigned long divisor) {
+    return (num + (divisor/2)) / divisor;
+}
+
+int GetFreeSizeInMB(const std::string &path)
+{
+    struct statvfs diskData;
+    statvfs(path.c_str(), &diskData);
+    unsigned long available = diskData.f_bavail * diskData.f_bsize;
+    if(access(path.c_str(), W_OK) == 0) {
+        return rounddiv(available, 1024*1024);
+    } else {
+        return 0;
+    }
+}
+#endif
+
+int main(int argc, char *argv[]) {
+    std::string path = ".";
+    if (argc > 1) { path = argv[1]; }
+    int freeSizeMb = GetFreeSizeInMB(path);
+    std::string val = std::to_string(freeSizeMb);
+
+    std::cout << "Free size is: " << val<< " MB.\n" << "In path: '" << path << "'." << std::endl;
     return 0;
 }
